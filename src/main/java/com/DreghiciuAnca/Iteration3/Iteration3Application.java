7b9a5e9563e86469707ae1c10c863f7a947754b2
DreghiciuAnca <anca_dreghiciu_1997@yahoo.com>
2020-05-28 12:07:06 +0300

    application main

---
+++ src/main/java/com/DreghiciuAnca/Iteration3/Iteration3Application.java
@@ -0,0 +1,13 @@
+package com.DreghiciuAnca.Iteration3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iteration3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iteration3Application.class, args);
+	}
+
+}
